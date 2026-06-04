d1dffe10f407284b34bf22388c8ef72103e7d86a
Syed Fahad <fahadrasheedsyed@gmail.com>
2026-06-04 09:57:49 -0400

    Initial Commit

---
+++ ScavengerHuntApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScavengerHuntApp
+//
+//  Created by Syed Fahad Rasheed on 2026-06-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScavengerHuntApp/ScavengerHuntAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScavengerHuntAppApp.swift
+//  ScavengerHuntApp
+//
+//  Created by Syed Fahad Rasheed on 2026-06-04.
+//
+
+import SwiftUI
+
+@main
+struct ScavengerHuntAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
